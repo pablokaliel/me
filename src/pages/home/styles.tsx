857ad3461ef40bf64f2d31ae4df3f70095c3f8e2
Pablo Kaliel <pablo.akyell.441@gmail.com>
2023-11-23 16:42:05 -0300

    removing comments in code

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -430,6 +430,11 @@ export const ArticleText = styled.div`
     left: 1.5rem;
     position: absolute;
     text-align: left;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 `;
 
